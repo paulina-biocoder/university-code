14e1418847d572d5cdaf1f389c634bc1787667fb
Paulina Kusztelak <117985065+paulina-biocoder@users.noreply.github.com>
2023-05-16 22:41:25 +0200

    Add files via upload

---
+++ palindrom.py
@@ -0,0 +1,19 @@
+'''Program pobiera od użytkownika słowo, następnie zamienia je na małe litery, potem sprawdza długość tekstu i oblicza połowę tej długości - są one potrzebne do określenia iteracji w pętli for.
+Potem za pomocą pętli sprawdzamy, czy znak pozycji w tekście nie jest równy długości - pozycja znaku - 1.
+Jeśli nie są sobie równe - wyraz nie jest palindromem, funkcją break kończymy działanie pętli i następnie program wypisuje czy słowo jest palindromem, czy nie'''
+
+tekst = input('Podaj tekst: ')
+tekst = tekst.lower()  # Zamiana na małe litery dla porównywania bez rozróżniania wielkości
+dlugosc = len(teskt)
+polowa_dlugosci = dlugosc // 2  # Obliczenie połowy długości tekstu
+
+palindrom = True
+for i in range(polowa_dlugosci):
+    if tekst[i] != tekst[dlugosc - i - 1]:
+        palindrom = False
+        break
+
+if palindrom:
+    print('Podany tekst jest palindromem.')
+else:
+    print('Podany tekst nie jest palindromem.')
