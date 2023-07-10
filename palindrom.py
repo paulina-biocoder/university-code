7b2a9e39a202922a60b6e9602e4a9af49fcdc468
Paulina Kusztelak <117985065+paulina-biocoder@users.noreply.github.com>
2023-07-10 16:04:49 +0200

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,19 +1,25 @@
-'''Program pobiera od użytkownika słowo, następnie zamienia je na małe litery, potem sprawdza długość tekstu i oblicza połowę tej długości.
-Potem za pomocą pętli sprawdzamy, czy znak pozycji w tekście nie jest równy długości - pozycja znaku - 1.
-Jeśli nie są sobie równe - wyraz nie jest palindromem, funkcją break kończymy działanie pętli i następnie program wypisuje czy słowo jest palindromem, czy nie'''
+'''Dana jest pusta lista X, program najpierw pobiera od użytkownika liczbę elementów tej listy, 
+a następnie za pomocą pętli for pobiera odpowiednią ilośĆ jej elementów będących liczbami całkowitymi.
+Przy użyciu kolejnej pętli for program sprawdza odpowiednio i-ty element tej listy oraz element od końca odpowiadający pozycji i-tego elementu. 
+Jeśli elementy te są równe, lista jest palindromiczna, jeśli nie, nie jest palindromiczna.
+'''
 
-tekst = input('Podaj tekst: ')
-tekst = tekst.lower() 
-dlugosc = len(teskt)
-polowa_dlugosci = dlugosc // 2 
+X = []
+n = int(input('Podaj ilośc elementów listy X: '))
 
-palindrom = True
-for i in range(polowa_dlugosci):
-    if tekst[i] != tekst[dlugosc - i - 1]:
-        palindrom = False
+for i in range(n):
+    liczba = int(input('Podaj liczbę, która ma zawierać się w liście X: '))
+    X.append(liczba)
+
+for i in range(len(X) // 2):
+    if X[i] != X[-(i+1)]:
+        wynik = 'nie jest'
+        print(X[i], X[-(i+1)])
         break
+    else:
+        wynik = 'jest'
+    print(X[i], X[-(i+1)])
 
-if palindrom:
-    print('Podany tekst jest palindromem.')
-else:
-    print('Podany tekst nie jest palindromem.')
+komunikat = 'Podana lista {} palindromiczna'
+
+print(komunikat.format(wynik))
