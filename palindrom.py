355fe98985af78c25898a580b9ad885d3bdea63e
Paulina Kusztelak <117985065+paulina-biocoder@users.noreply.github.com>
2023-07-11 14:49:52 +0200

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,4 +1,5 @@
-'''Dana jest pusta lista X, program najpierw pobiera od użytkownika liczbę elementów tej listy, 
+'''
+Dana jest pusta lista X, program najpierw pobiera od użytkownika liczbę elementów tej listy, 
 a następnie za pomocą pętli for pobiera odpowiednią ilośĆ jej elementów będących liczbami całkowitymi.
 Przy użyciu kolejnej pętli for program sprawdza odpowiednio i-ty element tej listy oraz element od końca odpowiadający pozycji i-tego elementu. 
 Jeśli elementy te są równe, lista jest palindromiczna, jeśli nie, nie jest palindromiczna.
